e246933a06ba548955674ff337e5921696d6094c
Hermanjoyt <hermankamulegeya18@gmail.com>
2026-05-03 10:57:30 +0300

    Add settings configuration in settingss.js

---
+++ settingss.js
@@ -0,0 +1,12 @@
+export default {
+  SESSION_ID: "",
+
+  OWNER_NUMBER: "256726212154",
+  OWNER_NAME: "KHAREL HERMAN 🇺🇬",
+
+  PREFIX: ".",
+
+  BOT_NAME: "BATTLE MD",
+
+  TIMEZONE: "Africa/Harare"
+};
